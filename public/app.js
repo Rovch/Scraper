fd8c32889125094c752bb446ffd7ed5d0a591f00
Rovch <wesleyjacksonjr33@hotmail.com>
2018-06-07 00:27:53 -0500

    testing

--- public/app.js
+++ public/app.js
@@ -5,8 +5,8 @@ $.getJSON("/articles", function (data) {
   console.log(current)
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    $("#articles").append(`<a href="${data[i]._id}" id="link" data-id="${data[i]._id}> data[i].link </>`)
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />"  + "</p>");
+    $("#articles").append(`<a href="${data[i]._id}" id="link" data-id="${data[i]._id}> ${data[i]._id} </>`)
     $("#articles").append(`<button id="edit" data-id="${data[i]._id}">Edit </button>`)
   }
 });
